ba8e466c0f0957be0f5ab51c9db2569616af4ddb
Yunya Hsu <yunyahsu110@gmail.com>
2023-02-05 13:54:00 +0800

    feat: create login API

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,11 +1,58 @@
+// utils
+require('dotenv').config();
+
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+// interface
+interface UserData {
+  id: number;
+  account: string;
+  password: string;
+  name: string | null;
+  birthday: string | null;
+};
+interface TokenUserData {
+  id: number;
+  account: string;
+  name: string | null;
+  birthday: string | null;
+};
+
+// moke data
+const users: UserData[] = require('../../user-data.json');
 
 
 // resolvers
 export const resolvers = {
-
   Query: {
-    test: () => {
-      return 'Hello World!!!!'
-    }
+    login: async(_parent: any, args: { account: string, password: string }) => {
+      const { account, password }: { account: string, password: string } = args;
+
+      // find user in database
+      const theUser = users.find(user => user.account === account);
+      if (!theUser) throw new Error('Email Account Not Exists');
+
+      // verify password
+      const verifyResult = await argon2.verify(theUser.password, password);
+      if (!verifyResult) throw new Error('Wrong Password');
+
+      // generate JWT
+      const token: TokenUserData = await jwt.sign(
+        {
+          id: theUser.id,
+          account: theUser.account,
+          name: theUser.name,
+          birthday: theUser.birthday
+        },
+        process.env.JWT_secret,
+        { expiresIn: process.env.JWT_expire }
+      );
+
+      // return JWT
+      return { token };
+    },
+
+
   }
 };
